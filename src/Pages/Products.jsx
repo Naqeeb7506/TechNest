6c18d1d87c83e2d2cf492a22c6025db996712d51
Mohammed Naqeeb <mohammed.naqeeb@binghatti.com>
2025-09-18 10:27:33 +0400

    Updated project with latest changes

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Filters from "../components/Filters/Filters";
 import Sort from "../components/Sort/Sort";
 import ProductList from "../components/ProductList/ProductList";
-import "../assets/css/products.css";
+import "../assets/css/product.css";
 
 const Products = () => {
   return (
